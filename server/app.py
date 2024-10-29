d876a0a2c8d767e916668d8f52dba256fc8bbe76
John Mark Harrell <hijohnmark@gmail.com>
2024-10-29 14:37:36 +0700

    complete chatterbox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,85 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        
+        response = make_response(
+            messages,
+            200
+        )
+    
+        return response
+    
+    if request.method == 'POST':
+        new_message = Message(
+            body=request.get_json().get("body"),
+            username=request.get_json().get("username")
+        )
 
-@app.route('/messages/<int:id>')
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )    
+
+        return response
+    
+    elif request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json().get(attr))
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+        
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
